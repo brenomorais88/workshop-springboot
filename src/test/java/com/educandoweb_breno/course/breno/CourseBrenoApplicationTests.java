04d3b994965db18e2a3c17dfb8b6325e436b1594
Breno <brenomorais88@gmail.com>
2023-02-04 17:09:41 -0300

    Criando projeto inicial

---
+++ src/test/java/com/educandoweb_breno/course/breno/CourseBrenoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.educandoweb_breno.course.breno;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourseBrenoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
